b969e5333cb72fad5ea6d955c3bbb003af11e21a
inkook <kik1112@naver.com>
2022-06-14 17:58:29 +0900

    category와 product vo구현

---
+++ src/main/java/vo/Product.java
@@ -0,0 +1,103 @@
+package vo;
+
+import java.util.Date;
+
+public class Product {
+
+	private int no;
+	private String name;
+	private String imageName;
+	private String description;
+	private int customerPrice;
+	private int sellPrice;
+	private int depositPoint;
+	private int deliveryFee;
+	private int stock;
+	private String onSell;
+	private Date createdDate;
+	private Date updatedDate;
+	private int categoryNo;
+	
+	public Product() {}
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getImageName() {
+		return imageName;
+	}
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getCustomerPrice() {
+		return customerPrice;
+	}
+	public void setCustomerPrice(int customerPrice) {
+		this.customerPrice = customerPrice;
+	}
+	public int getSellPrice() {
+		return sellPrice;
+	}
+	public void setSellPrice(int sellPrice) {
+		this.sellPrice = sellPrice;
+	}
+	public int getDepositPoint() {
+		return depositPoint;
+	}
+	public void setDepositPoint(int depositPoint) {
+		this.depositPoint = depositPoint;
+	}
+	public int getDeliveryFee() {
+		return deliveryFee;
+	}
+	public void setDeliveryFee(int deliveryFee) {
+		this.deliveryFee = deliveryFee;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public String getOnSell() {
+		return onSell;
+	}
+	public void setOnSell(String onSell) {
+		this.onSell = onSell;
+	}
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+	public int getCategoryNo() {
+		return categoryNo;
+	}
+	public void setCategoryNo(int categoryNo) {
+		this.categoryNo = categoryNo;
+	}
+	
+	
+}
