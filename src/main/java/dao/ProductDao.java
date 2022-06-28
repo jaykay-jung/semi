d0114c0e749f5b9722aab53ffb6612cec360686b
inkook <kik1112@naver.com>
2022-06-29 07:48:33 +0900

    사진 추가, 검색기능 완전히 구현, 상세정보에 표시될 이미지리스트 작업중, 커밋하지는 않았지만 q&a 기능 작성중

--- src/main/java/dao/ProductDao.java
+++ src/main/java/dao/ProductDao.java
@@ -21,6 +21,30 @@ public class ProductDao {
 	
 	private DaoHelper helper = DaoHelper.getInstance();
 	
+	
+	// (소연님 추가요청) 상품 리스트 조회
+	public List<Product> getProductList() throws SQLException {
+        String sql = "select * "
+                 + "from semi_products "
+                 + "order by product_no asc ";
+        return helper.selectList(sql, rs -> {
+           Product product = new Product();
+           product.setNo(rs.getInt("product_no"));
+           product.setName(rs.getString("product_name"));
+           product.setImageName(rs.getString("product_image_name"));
+           product.setDescription(rs.getString("product_description"));
+           product.setCustomerPrice(rs.getInt("product_customer_price"));
+           product.setSellPrice(rs.getInt("product_sell_price"));
+           product.setDepositPoint(rs.getInt("product_deposit_point"));
+           product.setDeliveryFee(rs.getInt("product_delivery_fee"));
+           product.setStock(rs.getInt("product_stock"));
+           product.setOnSell(rs.getString("product_on_sell"));
+           product.setCreatedDate(rs.getDate("product_created_date"));
+           product.setUpdatedDate(rs.getDate("product_updated_date"));
+           product.setCategoryNo(rs.getInt("category_no"));
+           return product;
+        });
+     }
 
 	
 	// 상품 관리자 페이지에서 상품 정보 등록 (완료, 데이터로 확인x)
@@ -56,7 +80,7 @@ public class ProductDao {
 	}
 	
 	
-	// 상품 번호를 이용해서 등록된 상품의 모든 정보 조회하여 product에 담기  (이미지 4개에 대한 처리 고민))
+	// 상품 번호를 이용해서 등록된 상품의 모든 정보 조회하여 product에 담기
 	public Product getProductByNo(int product_no) throws SQLException {
 	String sql = "select p.product_no, p.product_name, p.product_image_name, p.product_description, p.product_customer_price, p.product_sell_price, p.product_deposit_point, p.product_delivery_fee, p.product_stock, p.product_on_sell, p.product_deleted, p.product_created_date, p.product_updated_date, c.category_no, c.category_name "      
 			   + "from semi_products p, semi_product_category c  "	
@@ -215,7 +239,7 @@ public class ProductDao {
 				product.setDescription(rs.getString("product_description"));
 				
 				return product;
-			}, beginIndex, endIndex, keyword);
+			}, keyword, beginIndex, endIndex);
 		}
 	
 		//특정 키워드(상품 이름)에 따른 카테고리별 상품 목록 조회 출력 (해당 인덱스에 해당하는 상품들 출력, 카테고리 명칭 포함) 
---
+++ src/main/java/vo/ImageList.java
@@ -0,0 +1,46 @@
+package vo;
+
+public class ImageList {
+
+	private int imageNo;
+	private String imageName;
+	private int productName;
+	private int categoryName;
+	
+	public ImageList() {}
+
+	public int getImageNo() {
+		return imageNo;
+	}
+
+	public void setImageNo(int imageNo) {
+		this.imageNo = imageNo;
+	}
+
+	public String getImageName() {
+		return imageName;
+	}
+
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+
+	public int getProductName() {
+		return productName;
+	}
+
+	public void setProductName(int productName) {
+		this.productName = productName;
+	}
+
+	public int getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(int categoryName) {
+		this.categoryName = categoryName;
+	}
+	
+	
+	
+}
